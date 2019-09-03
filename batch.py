54a5f5bdc8905bcb3c3f82765ac9913f9e3bc993
lallinger-arbeit <patrick.lallinger@rohde-schwarz.com>
2019-09-03 13:28:43 +0200

    add

--- batch.py
+++ batch.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from __future__ import print_function
+exec('from __future__ import print_function')
 
 
 import os, json, uuid, sys
