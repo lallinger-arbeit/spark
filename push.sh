fa356c9577fb26efc7bbbad32e3ee838242461b3
lallinger-arbeit <54403915+lallinger-arbeit@users.noreply.github.com>
2019-09-09 07:30:42 +0200

    Delete push.sh

--- push.sh
+++
@@ -1,5 +0,0 @@
-docker build -t lallinger/batch . --build-arg HTTP_PROXY=$http_proxy --build-arg HTTPS_PROXY=$http_proxy
-docker push lallinger/batch
-oc project spark-operator
-oc delete -f spark.yaml
-oc create -f spark.yaml
