63a6a3bc8f0c7041db8b49433193802a684b2963
lallinger-arbeit <patrick.lallinger@rohde-schwarz.com>
2019-09-10 10:28:07 +0200

    test

--- batch.py
+++ batch.py
@@ -22,6 +22,7 @@ from random import random
 from operator import add
 
 from pyspark.sql import SparkSession
+from pyspark import SparkContext
 from azure.storage.blob import BlockBlobService, PublicAccess
 
 if __name__ == "__main__":
@@ -36,26 +37,33 @@ if __name__ == "__main__":
     
     user=os.environ['STORAGE_USERNAME']
     password=os.environ['STORAGE_PASSWORD']
-    container = "staging"
+    containerStaging = "staging"
+    containerAnalytics = "analytics"
     
     block_blob_service = BlockBlobService(account_name=user, account_key=password)
 
+    blobs = []
 
     print("\nList blobs in the container")
-    generator = block_blob_service.list_blobs(container)
+    generator = block_blob_service.list_blobs(containerStaging)
     for blob in generator:
         print("\t Blob name: " + blob.name)
+        blob.append(blob.name)
 
 
-    partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+    partitions = 4
     n = 100000 * partitions
 
-    def f(_):
-        x = random() * 2 - 1
-        y = random() * 2 - 1
-        return 1 if x ** 2 + y ** 2 <= 1 else 0
+    def getCount(blob):
+        filepath = os.path.join("~/tmp", blob)
+        block_blob_service.get_blob_to_path(containerStaging, blob, filepath)
 
-    count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
+        dataframe = spark.read.csv(filepath, mode="DROPMALFORMED",schema="name,count,meta")
+        print(dataframe.select("count").collect())
+
+        return dataframe.select("count").collect()
+
+    sum = spark.sparkContext.parallelize(blobs, partitions).map(getCount).reduce(add)
+    print("Sum is: " % sum)
 
     spark.stop()
